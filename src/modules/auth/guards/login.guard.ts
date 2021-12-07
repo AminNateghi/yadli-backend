bcb001bf9a21f06b52592c6dd51cf1a8747accab
Amin Nateghi <amin.programmer@gmail.com>
2021-12-07 03:01:50 +0300

    remove login guard

--- src/modules/auth/guards/login.guard.ts
+++ src/modules/auth/guards/login.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LoginGuard extends AuthGuard('local') {
+export class LoginGuard extends AuthGuard('jwt') {
   async canActivate(context: ExecutionContext) {
     const result = (await super.canActivate(context)) as boolean;
     const request = context.switchToHttp().getRequest();
